b284ed1fd0c5bd285281aba2cf376f3612c3d27a
Noah Ryan <nsmryan@gmail.com>
2021-02-21 07:49:38 -0500

    Modified for my liking

--- dwmstatus.c
+++ dwmstatus.c
@@ -17,11 +17,9 @@
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
-char *tzutc = "UTC";
-char *tzberlin = "Europe/Berlin";
 
 static Display *dpy;
+
 
 char *
 smprintf(char *fmt, ...)
@@ -179,13 +177,13 @@ int
 main(void)
 {
 	char *status;
-	char *avgs;
+	//char *avgs;
 	char *bat;
 	char *bat1;
-	char *tmar;
-	char *tmutc;
-	char *tmbln;
-	char *t0, *t1, *t2;
+	//char *tmar;
+	//char *tmutc;
+	char *timestr;
+	//char *t0, *t1, *t2;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
@@ -193,30 +191,31 @@ main(void)
 	}
 
 	for (;;sleep(60)) {
-		avgs = loadavg();
+		//avgs = loadavg();
 		bat = getbattery("/sys/class/power_supply/BAT0");
 		bat1 = getbattery("/sys/class/power_supply/BAT1");
-		tmar = mktimes("%H:%M", tzargentina);
-		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
-		t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
-		t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
-		t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
+		timestr = mktimes("%a %d %b %H:%M %Z %Y", "");
 
-		status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
-				t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
-				tmbln);
+        // not working, so removed
+		//t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
+		//t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
+		//t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
+		//status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
+		//		t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
+		//		tmbln);
+
+		status = smprintf("L:%s B:%s %s %s", bat, bat1, timestr);
 		setstatus(status);
 
-		free(t0);
-		free(t1);
-		free(t2);
-		free(avgs);
+		//free(t0);
+		//free(t1);
+		//free(t2);
+		//free(avgs);
 		free(bat);
 		free(bat1);
-		free(tmar);
-		free(tmutc);
-		free(tmbln);
+		//free(tmar);
+		//free(tmutc);
+		free(timestr);
 		free(status);
 	}
 
